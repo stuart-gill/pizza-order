802487b41a57ab5ef268129bb37052a76711a15e
Stuart Gill <stuart.a.gill@gmail.com>
2018-11-02 10:21:00 -0700

    Introduce functioning pizza constructor as well as an add toppings prototype, integrated with html

--- js/scripts.js
+++ js/scripts.js
@@ -38,10 +38,14 @@
 // }
 
 // Business Logic for Pizzas ---------
-function Pizza(firstName, size, toppings) {
+function Pizza(firstName, size) {
   this.firstName = firstName,
   this.size = size,
-  this.toppings = toppings
+  this.toppings = new Array();
+}
+
+Pizza.prototype.addToppings = function(topping){
+  this.toppings.push(topping);
 }
 
 Pizza.prototype.price = function() {
@@ -62,75 +66,78 @@ Pizza.prototype.price = function() {
 
 
 // User Interface Logic ---------
-var addressBook = new AddressBook();
+// var addressBook = new AddressBook();
 
-function displayContactDetails(addressBookToDisplay) {
-  var contactsList = $("ul#contacts");
-  var htmlForContactInfo = "";
-  addressBookToDisplay.contacts.forEach(function(contact) {
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
-  });
-  contactsList.html(htmlForContactInfo);
-};
+// function displayContactDetails(addressBookToDisplay) {
+//   var contactsList = $("ul#contacts");
+//   var htmlForContactInfo = "";
+//   addressBookToDisplay.contacts.forEach(function(contact) {
+//     htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+//   });
+//   contactsList.html(htmlForContactInfo);
+// };
 
-function showContact(contactId) {
-  var contact = addressBook.findContact(contactId);
-  $("#show-contact").show();
-  $(".first-name").html(contact.firstName);
-  $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.phoneNumber);
-  $(".email").html(contact.email);
-  $(".home-address").html(contact.address.homeAddress);
-  $(".work-address").html(contact.address.workAddress);
+// function showContact(contactId) {
+//   var contact = addressBook.findContact(contactId);
+//   $("#show-contact").show();
+//   $(".first-name").html(contact.firstName);
+//   $(".last-name").html(contact.lastName);
+//   $(".phone-number").html(contact.phoneNumber);
+//   $(".email").html(contact.email);
+//   $(".home-address").html(contact.address.homeAddress);
+//   $(".work-address").html(contact.address.workAddress);
 
-  var buttons = $("#buttons");
-  buttons.empty();
-  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
-}
+//   var buttons = $("#buttons");
+//   buttons.empty();
+//   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+// }
 
-function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
-    showContact(this.id);
-  });
+// function attachContactListeners() {
+//   $("ul#contacts").on("click", "li", function() {
+//     showContact(this.id);
+//   });
 
-  $("#buttons").on("click", ".deleteButton", function() {
-    addressBook.deleteContact(this.id);
-    $("#show-contact").hide();
-    displayContactDetails(addressBook);
-  });
+//   $("#buttons").on("click", ".deleteButton", function() {
+//     addressBook.deleteContact(this.id);
+//     $("#show-contact").hide();
+//     displayContactDetails(addressBook);
+//   });
 
-};
+// };
+var newPizza = new Pizza();
 
 $(document).ready(function() {
-  attachContactListeners();
-  $("form#new-contact").submit(function(event) {
+  // attachContactListeners();
+  $("form#new-pizza").submit(function(event) {
     event.preventDefault();
     var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedPhoneNumber = $("input#new-phone-number").val();
-    var inputtedEmail = $("input#new-email").val();
-    var inputtedHomeAddress = $("input#new-home-address").val();
-    var inputtedWorkAddress = $("input#new-work-address").val();
-
-    if (inputtedHomeAddress === ""){
-      $("p").remove("#hAddress");
-    }
-    if (inputtedWorkAddress === ""){
-      $("p").remove("#wAddress");
-    }
-
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-phone-number").val("");
-    $("input#new-email").val("");
-    $("input#new-home-address").val("");
-    $("input#new-work-address").val("");
-
-    var newAddress = new Address(inputtedHomeAddress, inputtedWorkAddress);
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, newAddress);
-
-    addressBook.addContact(newContact);
-    displayContactDetails(addressBook);
+    newPizza.firstName = inputtedFirstName;
+    var inputtedSize = $("input[type=radio]:checked").val();
+    newPizza.size = inputtedSize;
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var inputtedTopping = $(this).val();
+      newPizza.addToppings(inputtedTopping);
+    });
   });
 })
+
+    // if (inputtedHomeAddress === ""){
+    //   $("p").remove("#hAddress");
+    // }
+    // if (inputtedWorkAddress === ""){
+    //   $("p").remove("#wAddress");
+    // }
+
+
+    // $("input#new-first-name").val("");
+    // $("input#new-last-name").val("");
+    // $("input#new-phone-number").val("");
+    // $("input#new-email").val("");
+    // $("input#new-home-address").val("");
+    // $("input#new-work-address").val("");
+
+    // var newAddress = new Address(inputtedHomeAddress, inputtedWorkAddress);
+    // var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, newAddress);
+
+    // addressBook.addContact(newContact);
+    // displayContactDetails(addressBook);
