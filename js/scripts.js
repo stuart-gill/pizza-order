489005861170b6ebabbc147471f99c1f31a448fc
Stuart Gill <stuart.a.gill@gmail.com>
2018-11-02 14:01:42 -0700

    Further streamline code

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,37 @@
 // Business Logic for Pizzas ---------
 function Pizza(firstName, size) {
   this.firstName = firstName,
-  this.size = size,
-  this.toppings = new Array();
+    this.size = size,
+    this.toppings = new Array();
 }
 
-Pizza.prototype.addToppings = function(topping){
+Pizza.prototype.addToppings = function (topping) {
   this.toppings.push(topping);
 }
 
-Pizza.prototype.price = function() {
+Pizza.prototype.price = function () {
   var toppingQuant = this.toppings.length;
-  var toppingPrice= toppingQuant*1.99;
-  var sizePrice=0;
-  if (this.size="small"){
-    sizePrice=5.99;
+  var toppingPrice = toppingQuant * 1.99;
+  var sizePrice = 0;
+  if (this.size = "small") {
+    sizePrice = 5.99;
   }
-  else if (this.size="medium"){
-    sizePrice=7.99;
+  else if (this.size = "medium") {
+    sizePrice = 7.99;
   }
   else {
-    sizePrice=9.99;
+    sizePrice = 9.99;
   }
-  return sizePrice+toppingPrice;
+  return sizePrice + toppingPrice;
 }
 
-function arrayToSentence( array, oxfordComma){
-  if( array.length > 1 ){
+function arrayToSentence(array, oxfordComma) {
+  if (array.length > 1) {
     var lastWord = " and " + array.pop();
-    if( oxfordComma && array.length > 1 ){
+    if (oxfordComma && array.length > 1) {
       lastWord = "," + lastWord;
     }
-  }else{
+  } else {
     var lastWord = "";
   }
   return array.join(", ") + lastWord;
@@ -48,15 +48,14 @@ function showPizza(pizza) {
   $("#pizza-text").html(pizza.firstName + ", your " + pizza.size + " pizza with " + toppingsString + " will cost $" + pizza.price() + ". Thank you for your order!");
 }
 
-$(document).ready(function() {
-  // attachContactListeners();
-  $("form#new-pizza").submit(function(event) {
+$(document).ready(function () {
+  $("form#new-pizza").submit(function (event) {
     event.preventDefault();
     var inputtedFirstName = $("input#new-first-name").val();
     newPizza.firstName = inputtedFirstName;
     var inputtedSize = $("input[type=radio]:checked").val();
     newPizza.size = inputtedSize;
-    $("input:checkbox[name=toppings]:checked").each(function(){
+    $("input:checkbox[name=toppings]:checked").each(function () {
       var inputtedTopping = $(this).val();
       newPizza.addToppings(inputtedTopping);
     });
